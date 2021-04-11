be59aadb2465075ce5dc3b3024793fa6409c1afc
Asad-Bin <66724153+Asad-Bin@users.noreply.github.com>
2021-04-11 19:03:52 +0600

    Create 12703 - Little Rakin.cpp

---
+++ UVa/12703 - Little Rakin.cpp
@@ -0,0 +1,155 @@
+//...Bismillahir Rahmanir Rahim...
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// typedefs...
+typedef long long            ll;
+typedef unsigned long long   ull;
+typedef pair<int, int>       pii;
+typedef vector<int>          vi;
+typedef vector<ll>           vl;
+typedef pair<ll, ll>         pll;
+ 
+// constants...
+//const double PI = acos(-1);
+const int mod = 1e9+7; // 998244353;
+const int MXI = 1e9+5;
+const ll  MXL = 1e18+5;
+ 
+// defines...
+#define MP             make_pair
+#define PB             push_back
+#define fi             first
+#define se             second
+#define sz(x)          (int)x.size()
+#define all(x)         begin(x), end(x)
+#define si(a)          scanf("%d", &a)
+#define sii(a, b)      scanf("%d%d", &a, &b)
+#define siii(a, b, c)  scanf("%d%d%d", &a, &b, &c)
+#define precise_impact cout<<setprecision(6)<<fixed;
+ 
+// chess moves...
+// int dx[] = {+0, +0, -1, +1}; ///Up-down, Left-Right
+// int dy[] = {+1, -1, +0, +0};
+// int dx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's Move
+// int dy[] = {-1,+1,+0,+0,+1,+1,-1,-1};
+// int dx[] = {-2, -2, -1, -1,  1,  1,  2,  2}; ///Knight's Move
+// int dy[] = {-1,  1, -2,  2, -2,  2, -1,  1};
+
+// structs...
+struct hash_pair{ template <class T1, class T2> size_t operator()(const pair<T1, T2>& p) const{ auto hash1 = hash<T1>{}(p.first); auto hash2 = hash<T2>{}(p.second); return hash1 ^ hash2;} };
+
+// functions...
+ll gcd(ll a, ll b){ while (b){ a %= b; swap(a, b);} return a;}
+ll lcm(ll a, ll b){ return (a/gcd(a, b)*b);}
+ll bigmod(ll a, ll p, ll M)
+{
+    // If 'M' is a prime number, then we can speed up it just by calculating "a^(p%(M-1))";
+	p %= (M-1);
+	
+    ll result = 1LL;
+    while(p>0LL){
+        if(p&1LL) result = result * a % M;
+        a = a * a % M;
+        p>>=1LL;
+    }
+    return result;
+}
+ll inverse(ll a, ll M){
+    if(gcd(a, M)==1) return bigmod(a, M-2, M) % M; // (ax)%M = 1
+    return 1;
+}
+ll ncr(ll a, ll b){ ll x = max(a-b, b), ans=1; for(ll K=a, L=1; K>=x+1; K--, L++){ ans = ans * K % mod; ans = ans * inverse(L, mod) % mod;} return ans;}
+ll egcd(ll a,ll b,ll &x,ll &y){ if(a==0){ x=0; y=1; return b;} ll x1,y1; ll d=egcd(b%a,a,x1,y1); x=y1-(b/a)*x1; y=x1; return d;}
+
+
+
+vector<int> primes;
+vector<bool> mark(1002);
+void sieve(int n)
+{
+    int i, j, limit = sqrt(n*1.) + 2;
+
+    mark[1] = 1;
+
+    for(i = 4; i<=n; i+=2) mark[i] = 1;
+
+    primes.push_back(2);
+
+    for(i = 3; i <= n; i += 2)
+    {
+        if(!mark[i])
+        {
+            primes.push_back(i);
+
+            if(i<=limit)
+            {
+                for(j = i*i; j <= n; j += i*2)
+                {
+                    mark[j] = 1;
+                }
+            }
+        }
+    }
+}
+map<int, ll> divisors;
+void divs(int n, int x) {
+	int cnt, i;
+	
+	for(i=0; i<(int)primes.size() && (primes[i]*primes[i])<=n; i++) {
+		if(n%primes[i]==0) {
+			cnt=1;
+			
+			while(n%primes[i]==0) {
+				n/=primes[i];
+				cnt++;
+			}
+			
+			divisors[primes[i]] += (ll)x*(cnt-1);
+		}
+	}
+	
+	if(n>1) {
+		divisors[n] += x;
+	}
+}
+int F[45][2];
+void task()
+{
+	// Code here.. ..
+	
+	sieve(1000);
+	
+	F[0][0] = F[1][1] = 1;
+	F[0][1] = F[1][0] = 0;
+	
+	for(int K=2; K<=40; K++){
+		F[K][0] = F[K-1][0]+F[K-2][0];
+		F[K][1] = F[K-1][1]+F[K-2][1];
+	}
+	
+	int t, n, a, b;
+	
+	cin >> t;
+	while(t--){
+		cin >> n >> a >> b;
+		
+		divisors.clear();
+		divs(a, F[n][0]);
+		divs(b, F[n][1]);
+		
+		for(auto x:divisors) cout << x.fi << ' ' << x.se << "\n";
+		cout << "\n";
+	}
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    task();
+    return 0;
+}
